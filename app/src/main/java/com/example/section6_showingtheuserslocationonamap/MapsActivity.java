e5ae94101f16a1797f8cc7bc19e6eb65db3d2f6b
inkanusmaster <furymistrz@gmail.com>
2019-07-09 21:23:10 +0200

    Ruszyło. Teraz nawigację zrób

---
+++ app/src/main/java/com/example/section6_showingtheuserslocationonamap/MapsActivity.java
@@ -0,0 +1,32 @@
+package com.example.section6_showingtheuserslocationonamap;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); //czaderskie!
+        LatLng mtEverest = new LatLng(27.9, 86.9);
+        mMap.addMarker(new MarkerOptions().position(mtEverest).title("Gureczga").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mtEverest, 12)); //zoom od 1- do 20+
+    }
+}
